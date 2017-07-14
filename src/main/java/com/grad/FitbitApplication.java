ec64764d201e3a370b44c932e61a4be2aae911b2
nikos mas <n.mastrogiannopoulos@logicea.com>
2017-07-14 17:18:45 +0300

    minor fix

--- src/main/java/com/grad/FitbitApplication.java
+++ src/main/java/com/grad/FitbitApplication.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import com.grad.config.AuthorizationProperties;
+import com.grad.config.FitbitApiUrlProperties;
 import com.grad.config.MailInfoProperties;
 import com.grad.config.MongoProperties;
 import com.grad.config.RefreshTokenProperties;
 
 /**
- * @author nikos_mas
+ * @author nikos_mas, alex_kak
  */
 
 @EnableConfigurationProperties({ MongoProperties.class, AuthorizationProperties.class, MailInfoProperties.class,
-		RefreshTokenProperties.class })
+		RefreshTokenProperties.class, FitbitApiUrlProperties.class })
 @SpringBootApplication
 public class FitbitApplication {
 
